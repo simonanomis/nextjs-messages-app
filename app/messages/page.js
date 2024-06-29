f08e0683958f8319659a899485865a1a5f260a6e
OFFICE\simona.ristovska <simona.ristovska@it-labs.com>
2024-06-30 01:13:10 +0200

    different ways of controlling caching

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,12 @@
 import Messages from '@/components/messages';
+import {unstable_noStore} from "next/cache";
+
+//export const revalidate = 5;
+//export const dynamic = 'force-dynamic'; //same as cache: 'no-store'
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      revalidate: 5
-    },
-  });
+  unstable_noStore(); //disable cache for a specific component
+  const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
